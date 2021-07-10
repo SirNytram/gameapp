d339cb78926b5f3144603f497157499daa62ede0
NytramQC <30558613+NytramQC@users.noreply.github.com>
2021-07-09 23:49:29 -0400

    CH: no more init, setup for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gameapp',
+    version='1.0.0',
+    url='https://github.com/SirNytram/gameapp',
+    author='Martin Cote',
+    author_email='martcote@gmail.com',
+    description='game creation wrapper for pygame and pythonista',
+    packages=find_packages(),    
+    install_requires=[],
+)
