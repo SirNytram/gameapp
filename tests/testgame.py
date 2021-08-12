40f07d6b0b9cb93d9bf433e93d65cefbb7aacc99
NytramQC <30558613+NytramQC@users.noreply.github.com>
2021-08-12 01:59:04 -0400

    BF: add init file to treat tests folder as module BF: compilation errors for test project

--- tests/testgame.py
+++ tests/testgame.py
@@ -3,16 +3,16 @@ from gameapp import *
 
 class MyRedCar(GameImage):
     def __init__(self, parent):
-        super().__init__(parent, "gameapp/images/redcar.png", (200, 300))
+        super().__init__(parent, "gameapp/images/redcar.png", position=(200, 300))
         self.bRightSide = True
         self.t = Rect(0,0,0,0)
 
     def move(self):
         
         if self.bRightSide:
-            self.position.move_ip(3, 0)
+            self.position.x += 3
         else:
-            self.position.move_ip(-3, 0)
+            self.position.x -= 3
 
         if self.position.x < 0:
             self.position.x = 200
@@ -23,14 +23,14 @@ class MyRedCar(GameImage):
         self.bRightSide = not self.bRightSide
 
 
-class TestGane(GameApp):
+class TestGame(GameApp):
     def __init__(self):
         super().__init__() 
 
         self.width = 1000
         self.redcar = MyRedCar(self)
         self.fps = 50
-        self.bluecar = GameImage(self, 'gameapp/images/bluecar.png', (10,500))
+        self.bluecar = GameImage(self, 'gameapp/images/bluecar.png', position=(10,500))
         
         # self.redcar = 
         self.fontVerdana = GameFont(self, 'Verdana',50)
@@ -38,7 +38,7 @@ class TestGane(GameApp):
 
 
     def on_loop(self):
-        self.bluecar.position.move_ip(0, -5)
+        self.bluecar.position.y -= 5
         if self.bluecar.position.y < 5:
             self.bluecar.position.y  = 500
 
@@ -65,4 +65,4 @@ class TestGane(GameApp):
 
 
 if __name__ == "__main__" :
-    TestGane().start()
+    TestGame().start()
